c4235bd84919ce77eefdccf21a81f9ed716aa142
Mauricio Rivera <mauriver21@gmail.com>
2023-09-02 13:58:53 -0500

    Install basic dependencies.

--- apps/library-ui/src/App.tsx
+++ apps/library-ui/src/App.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@reactjs-ui/core';
+import { Select } from '@reactjs-ui/form-fields';
+
 export const App = () => {
-  return <>Hello World</>;
+  return (
+    <>
+      <Button>Hello World</Button>
+      <Select />
+    </>
+  );
 };

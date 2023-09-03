e6f338ba103032f0769192fa764a3978e52eadbd
Mauricio Rivera <mauriver21@gmail.com>
2023-09-03 18:00:11 -0500

    Test native pc method

--- packages/neu-wine-api/src/App.tsx
+++ packages/neu-wine-api/src/App.tsx
@@ -1,3 +1,15 @@
+import { computer } from '@neutralinojs/lib';
+import { useEffect } from 'react';
+
 export const App = () => {
+  const getMemoryInfo = async () => {
+    let memoryInfo = await computer.getMemoryInfo();
+    console.log(memoryInfo);
+  };
+
+  useEffect(() => {
+    getMemoryInfo();
+  }, []);
+
   return <>Hello World ssss</>;
 };

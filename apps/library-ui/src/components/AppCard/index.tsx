cbd9e68e435932f2d20e63babae1bf43f3143be9
Mauricio Rivera <mauriver21@gmail.com>
2024-02-04 23:51:55 -0500

    WIP AppCard component.

--- apps/library-ui/src/components/AppCard/index.tsx
+++ apps/library-ui/src/components/AppCard/index.tsx
@@ -1,7 +1,33 @@
-import { Card, CardProps } from '@reactjs-ui/core';
+import { Box, Button, Card, CardProps, Image } from '@reactjs-ui/core';
 
 export interface AppCardProps extends CardProps {}
 
 export const AppCard: React.FC<CardProps> = ({ ...rest }) => {
-  return <Card {...rest}>Ahora</Card>;
+  return (
+    <Card sx={{ width: 200, height: 280 }} {...rest}>
+      <Box
+        height="100%"
+        width="100%"
+        p={1}
+        display="grid"
+        gridTemplateRows="1fr 30px"
+        rowGap={'10px'}
+      >
+        <Image
+          src="https://petapixel.com/assets/uploads/2022/08/fdfs19.jpg"
+          height="100%"
+          style={{
+            objectFit: 'cover',
+            maxWidth: '100%',
+            borderRadius: 4,
+          }}
+        />
+        <Box>
+          <Button sx={{ width: 20 }} color="secondary">
+            A
+          </Button>
+        </Box>
+      </Box>
+    </Card>
+  );
 };

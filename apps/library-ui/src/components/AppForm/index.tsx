82f86539bfa3dd4de9e1e16e0daa44b2d46fe902
Mauricio Rivera <mauriver21@gmail.com>
2024-02-05 23:55:23 -0500

    Change create app ui design

--- apps/library-ui/src/components/AppForm/index.tsx
+++ apps/library-ui/src/components/AppForm/index.tsx
@@ -1,9 +1,9 @@
-import { Body1, Grid, Select, Stack, TextField } from '@reactjs-ui/core';
+import { Grid, H6, Select, Stack, TextField } from '@reactjs-ui/core';
 
 export const AppForm: React.FC = () => {
   return (
-    <Stack p={2} spacing={3}>
-      <Body1>Create App</Body1>
+    <Stack p={2} spacing={3} maxWidth={900} margin="auto">
+      <H6 fontWeight={500}>Create App</H6>
       <form>
         <Grid container spacing={3}>
           <Grid item xs={12}>
--- apps/library-ui/src/pages/CreateApp/index.tsx
+++ apps/library-ui/src/pages/CreateApp/index.tsx
@@ -1,12 +1,10 @@
 import { AppForm } from '@components';
-import { Box, Card } from '@reactjs-ui/core';
+import { Box } from '@reactjs-ui/core';
 
 export const CreateApp: React.FC = () => {
   return (
     <Box p={3}>
-      <Card sx={{ maxWidth: 650, margin: 'auto' }}>
-        <AppForm />
-      </Card>
+      <AppForm />
     </Box>
   );
 };

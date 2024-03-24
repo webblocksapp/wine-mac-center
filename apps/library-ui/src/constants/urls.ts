04e9893c6322a1b42770de788e1e50812d546bd5
Mauricio Rivera <mauriver21@gmail.com>
2024-03-24 14:03:51 -0500

    Change app configs path.

--- apps/library-ui/src/constants/urls.ts
+++ apps/library-ui/src/constants/urls.ts
@@ -1,2 +1,2 @@
 export const WINE_APPS_CONFIGS_URL =
-  'https://raw.githubusercontent.com/webblocksapp/wine-mac-center/master/packages/wine-apps-configs/src';
+  'https://raw.githubusercontent.com/webblocksapp/wine-mac-center/master/packages/wine-apps-configs/src/configs';

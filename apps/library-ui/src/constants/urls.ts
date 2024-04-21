aea1385c8fdcca48d93ca2545abcd9899a8524ff
webblocksapp <mauriver21@gmail.com>
2024-04-21 13:44:38 -0500

    Fix downloadables url

--- apps/library-ui/src/constants/urls.ts
+++ apps/library-ui/src/constants/urls.ts
@@ -1,7 +1,8 @@
 export const WINE_APPS_SRC_URL =
   'https://raw.githubusercontent.com/webblocksapp/wine-mac-center/master/packages/wine-apps-configs/src';
 export const WINE_APPS_CONFIGS_URL = `${WINE_APPS_SRC_URL}/configs`;
-export const WINE_APPS_SETUP_EXECUTABLES_URL = `${WINE_APPS_CONFIGS_URL}/setup-executables`;
+export const WINE_APPS_DONWLOADABLES_URL = `${WINE_APPS_SRC_URL}/downloadables`;
+export const WINE_APPS_SETUP_EXECUTABLES_URL = `${WINE_APPS_DONWLOADABLES_URL}/setup-executables`;
 
 export const DOWNLOADABLES_URLS: { [key: string]: string } = {
   '{{Steam}}': `${WINE_APPS_SETUP_EXECUTABLES_URL}/SteamSetup.exe`,

d135c240433cdb5c57617e5a797d6e62b35373f6
Mauricio Rivera <mauriver21@gmail.com>
2024-04-28 13:29:01 -0500

    Update extract wine engine sh

--- packages/neu-wine-api/Contents/Resources/bash/extractWineEngine.sh
+++ packages/neu-wine-api/Contents/Resources/bash/extractWineEngine.sh
@@ -1,4 +1,4 @@
-rm -R "$WINE_APP_ENGINE_PATH/"*;
+rm -rf "$WINE_APP_ENGINE_PATH/"*;
 tar -xf "$WINE_ENGINES_PATH/$WINE_ENGINE_VERSION.tar.7z" -C "$WINE_APP_PATH" -v;
 mv "$WINE_APP_PATH/wswine.bundle/"* "$WINE_APP_ENGINE_PATH";
-rm -r "$WINE_APP_PATH/wswine.bundle";
+rm -rf "$WINE_APP_PATH/wswine.bundle";

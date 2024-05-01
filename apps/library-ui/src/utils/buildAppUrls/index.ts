47fe08324ecdeb21e737604cc2f52216c076fe41
Mauricio Rivera <mauriver21@gmail.com>
2024-05-01 14:53:22 -0500

    Remove header images no cache

--- apps/library-ui/src/utils/buildAppUrls/index.ts
+++ apps/library-ui/src/utils/buildAppUrls/index.ts
@@ -7,7 +7,7 @@ export const buildAppUrls = (appConfig?: Partial<WineApp>) => {
   const URL = `${WINE_APPS_CONFIGS_URL}/${appConfig?.keyName || ''}`;
   const ASSETS_URL = `${URL}/assets`;
   return {
-    imgSrc: encodeURL(`${ASSETS_URL}/header.jpeg?cache=${uuid()}`),
+    imgSrc: encodeURL(`${ASSETS_URL}/header.jpeg`),
     iconURL: encodeURL(`${ASSETS_URL}/winemacapp.icns?cache=${uuid()}`),
     scriptUrl: encodeURL(
       `${URL}/versions/${appConfig?.version || ''}/index.json`,

9c1d4331d610813519d70ed2bf31974198754a5f
Mauricio Rivera <mauriver21@gmail.com>
2024-08-17 20:24:03 -0500

    Stable bundle mac app script.

--- apps/library-ui/scripts/bundleMacApp.ts
+++ apps/library-ui/scripts/bundleMacApp.ts
@@ -5,6 +5,7 @@ import {
   cpSync,
   copyFileSync,
   chmodSync,
+  renameSync,
 } from 'fs';
 import path from 'path';
 
@@ -14,25 +15,33 @@ const run = () => {
   const BINARIES_PATHS = [`${BUILD_PATH}/library-ui-mac_x64`];
   const EXTENSIONS_PATH = `${BUILD_PATH}/extensions`;
   const RESOURCES_NEU_PATH = `${BUILD_PATH}/resources.neu`;
+  const RESOURCES_PATH = `./Contents/Resources`;
 
   for (const BINARY of BINARIES_PATHS) {
     const SUFFIX = BINARY.split('_').pop();
     if (SUFFIX === undefined) continue;
     const APP_PATH = `${DIST_FOLDER_PATH}/${SUFFIX}/WineMacApps`;
+    const APP_DOTAPP_PATH = `${DIST_FOLDER_PATH}/${SUFFIX}/WineMacApps.app`;
     const APP_CONTENTS_PATH = `${APP_PATH}/Contents`;
     const APP_MACOS_PATH = `${APP_CONTENTS_PATH}/MacOS`;
-    const APP_RESOURCES_PATH = `${APP_CONTENTS_PATH}/Resources`;
 
     existsSync(APP_PATH) && rmSync(APP_PATH, { recursive: true });
+    existsSync(APP_DOTAPP_PATH) && rmSync(APP_DOTAPP_PATH, { recursive: true });
     mkdirSync(APP_PATH, { recursive: true });
     mkdirSync(APP_MACOS_PATH, { recursive: true });
-    mkdirSync(APP_RESOURCES_PATH, { recursive: true });
+    cpSync(EXTENSIONS_PATH, `${APP_MACOS_PATH}/extensions`, {
+      recursive: true,
+    });
+    cpSync(RESOURCES_PATH, `${APP_CONTENTS_PATH}/Resources`, {
+      recursive: true,
+    });
     cpSync(EXTENSIONS_PATH, `${APP_MACOS_PATH}/extensions`, {
       recursive: true,
     });
     copyFileSync(RESOURCES_NEU_PATH, `${APP_MACOS_PATH}/resources.neu`);
     copyFileSync(BINARY, `${APP_MACOS_PATH}/winemacapps`);
     chmodSync(`${APP_MACOS_PATH}/winemacapps`, '755');
+    renameSync(APP_PATH, `${APP_PATH}.app`);
   }
 };
 

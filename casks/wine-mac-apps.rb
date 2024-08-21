8ddcb266a6447d2a08009600e4210cc462111457
Mauricio Rivera <mauriver21@gmail.com>
2024-08-20 19:19:56 -0500

    Add wine mac apps cask

---
+++ casks/wine-mac-apps.rb
@@ -0,0 +1,8 @@
+cask "wine-mac-apps" do
+  version "1.0.0"
+  sha256 "319ad951f76a786dc1ede48add5fc6b6a4657ef3502dc933f2ff3e37b4fcbe67"
+  url "https://github.com/webblocksapp/wine-mac-center/releases/download/v1.0.0-alpha/Wine.Mac.Apps.x64.zip"
+  name "Wine Mac Apps"
+  desc "An example application for Homebrew"
+  app "Wine Mac Apps.app"
+end

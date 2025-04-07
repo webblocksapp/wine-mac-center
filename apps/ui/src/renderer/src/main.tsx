060c5a2ce36bf46fde54b2689d2ce5b816fdf571
Mauricio Rivera <mauriver21@gmail.com>
2025-04-06 22:36:04 -0500

    Remove mocks

--- apps/ui/src/renderer/src/main.tsx
+++ apps/ui/src/renderer/src/main.tsx
@@ -3,39 +3,29 @@ import { DialogFactoryProvider, ThemeProvider } from 'reactjs-ui-core';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '@store';
-import { isDev } from '@utils/isDev';
 import { App } from './App';
-import './main.css';
 import { AppSetup } from '@components/AppSetup';
 import { EnvProvider } from '@components/EnvProvider';
 import { NotificationsProvider } from '@components/NotificationsProvider';
 import { WineAppPipelineProvider } from '@components/WineAppPipelineProvider';
+import './main.css';
 
-const main = async () => {
-  if (isDev()) {
-    const { worker } = await import('./mocks/browser');
-    worker.start({ onUnhandledRequest: 'bypass' });
-  }
-
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ThemeProvider>
-      <Provider store={store}>
-        <NotificationsProvider>
-          <EnvProvider>
-            <WineAppPipelineProvider>
-              <BrowserRouter>
-                <AppSetup>
-                  <DialogFactoryProvider>
-                    <App />
-                  </DialogFactoryProvider>
-                </AppSetup>
-              </BrowserRouter>
-            </WineAppPipelineProvider>
-          </EnvProvider>
-        </NotificationsProvider>
-      </Provider>
-    </ThemeProvider>
-  );
-};
-
-main();
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ThemeProvider>
+    <Provider store={store}>
+      <NotificationsProvider>
+        <EnvProvider>
+          <WineAppPipelineProvider>
+            <BrowserRouter>
+              <AppSetup>
+                <DialogFactoryProvider>
+                  <App />
+                </DialogFactoryProvider>
+              </AppSetup>
+            </BrowserRouter>
+          </WineAppPipelineProvider>
+        </EnvProvider>
+      </NotificationsProvider>
+    </Provider>
+  </ThemeProvider>
+);
--- apps/ui/src/renderer/src/mocks/data/index.ts
+++
@@ -1,9 +0,0 @@
-import { wineApps } from './wineApps';
-import { wineAppsConfigs } from './wineAppsConfigs';
-import { wineAppsPipelines } from './wineAppsPipelines';
-
-export const data = {
-  wineApps,
-  wineAppsConfigs,
-  wineAppsPipelines,
-};

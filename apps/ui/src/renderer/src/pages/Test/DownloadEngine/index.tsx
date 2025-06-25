743f68b20b35fc2ebed6f4fec5248b49968bb361
Mauricio Rivera <mauriver21@gmail.com>
2025-06-24 20:31:03 -0500

    Add ENGINES_URLS example test

--- apps/ui/src/renderer/src/pages/Test/DownloadEngine/index.tsx
+++ apps/ui/src/renderer/src/pages/Test/DownloadEngine/index.tsx
@@ -2,7 +2,14 @@ import { useState } from 'react';
 import { useWineAppContext } from '@pages/Test';
 import { Code } from '@components/Code';
 
-const ENGINES_URLS = [];
+const ENGINES_URLS = {
+  'WS11WineCX64Bit23.6.0': [
+    'https://raw.githubusercontent.com/webblocksapp/wine-mac-center/master/packages/wine-apps-configs/src/downloadables/engines/WS11WineCX64Bit23.6.0/WS11WineCX64Bit23.6.0.tar_part.7z.001',
+    'https://raw.githubusercontent.com/webblocksapp/wine-mac-center/master/packages/wine-apps-configs/src/downloadables/engines/WS11WineCX64Bit23.6.0/WS11WineCX64Bit23.6.0.tar_part.7z.002',
+    'https://raw.githubusercontent.com/webblocksapp/wine-mac-center/master/packages/wine-apps-configs/src/downloadables/engines/WS11WineCX64Bit23.6.0/WS11WineCX64Bit23.6.0.tar_part.7z.003',
+    'https://raw.githubusercontent.com/webblocksapp/wine-mac-center/master/packages/wine-apps-configs/src/downloadables/engines/WS11WineCX64Bit23.6.0/WS11WineCX64Bit23.6.0.tar_part.7z.004'
+  ]
+};
 
 export const DownloadEngine: React.FC = () => {
   const { wineApp } = useWineAppContext();

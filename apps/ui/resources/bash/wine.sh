ea54b9a2af649948805eb6055264213b5a0f48ec
Mauricio Rivera <mauriver21@gmail.com>
2025-07-05 19:26:01 -0500

    Fix wine sh cd executable

--- apps/ui/resources/bash/wine.sh
+++ apps/ui/resources/bash/wine.sh
@@ -6,7 +6,14 @@ wine() {
     WINE_ARCH=wine64
   fi
 
-  "$WINE_APP_SCRIPTS_PATH/wineEnv.sh" $WINE_ARCH "$@"
+  EXE_PATH=$(printf "%b" "$1")   # unescape if needed
+  EXE_FLAGS="$2"
+
+  EXE_DIR=$(dirname "$EXE_PATH")
+  EXE_FILE=$(basename "$EXE_PATH")
+
+  cd "$EXE_DIR" || exit 1
+  "$WINE_APP_SCRIPTS_PATH/wineEnv.sh" $WINE_ARCH "$EXE_FILE" "$EXE_FLAGS"
 }
 
 wine "$@"
